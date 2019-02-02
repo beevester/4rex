94e35eeb84decb16608c18412f559b8e58375fdb
Nyiko Mdluli <nyikomd@miway.co.za>
2019-02-02 07:52:44 +0200

    created component for the login and profile

---
+++ frontend/src/app/components/password/request-rest/request-rest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-rest',
+  templateUrl: './request-rest.component.html',
+  styleUrls: ['./request-rest.component.css']
+})
+export class RequestRestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
