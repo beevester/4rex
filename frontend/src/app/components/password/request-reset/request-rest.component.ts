5eff9ec725cb601b96b6e17516de6dce5027f45f
Nyiko Mdluli <nyikomd@miway.co.za>
2019-02-06 05:42:17 +0200

    reset password

--- frontend/src/app/components/password/request-reset/request-rest.component.ts
+++ frontend/src/app/components/password/request-reset/request-rest.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpCallsService} from '../../../services/http-calls.service';
+import {SnotifyService} from 'ng-snotify';
 
 @Component({
   selector: 'app-request-rest',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestRestComponent implements OnInit {
 
-  constructor() { }
+  private form = {
+    email: null
+  };
+  constructor(private http: HttpCallsService, private notify: SnotifyService) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.http.sendPasswordResetLink(this.form).subscribe(
+        data => this.handleResponse(data['token']),
+      error => this.notify.error(error.error.error)
+    );
+  }
+
+  handleResponse(response) {
+    console.log(response);
+    this.form.email = null;
+  }
 }
--- frontend/src/app/components/password/response-reset/response-reset.component.ts
+++ frontend/src/app/components/password/response-reset/response-reset.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpCallsService} from '../../../services/http-calls.service';
 
 @Component({
   selector: 'app-response-reset',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponseResetComponent implements OnInit {
 
-  constructor() { }
+  private error = [];
+  public  form = {
+    email: null,
+    password: null,
+    resetToken: null
+  };
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpCallsService) {
+    route.queryParams.subscribe(params => {
+      this.form.resetToken = params['token'];
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.http.changePassword(this.form).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
   }
 
 }
